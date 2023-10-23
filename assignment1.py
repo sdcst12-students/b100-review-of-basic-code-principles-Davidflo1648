f48e9b664d024809e64ae3fc2e8048e3d0629a6f
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-23 13:12:43 -0700

    task1 finished

--- assignment1.py
+++ assignment1.py
@@ -13,3 +13,24 @@ Your program should ask the user for
 * The program will calculate the amount of interest earned and display it.
 * Appropriate formatting of the output is a requirement for this assignment
 """
+userInput = input("Will you enter the amount of time in years, months or days? ")
+P = float(input("Enter the initial investment: "))
+R = float(input("Enter the annual interest rate in percent form: ")) / 100
+T = float(input("Enter the length of time: "))
+
+if userInput == 'years':
+  T
+  I = P * R * T
+  print(f"The amount of simple interest is {I:.2f}")
+elif userInput == 'months':
+  T / 12
+  I = P * R * T 
+  print(f"The amount of simple interest is {I:.2f}")
+elif userInput == 'days':
+  T / 365
+  I = P * R * T
+  print(f"The amount of simple interest is {I:.2f}")
+
+#I = P * R * T
+
+#print(f"The amount of simple interest is {I:.2f}")
